88fc1ab70c7048faa44474673290a70d770db03a
CIT195 <122820747+Holmes1776@users.noreply.github.com>
2024-03-05 16:43:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ClubInterface
+{
+    interface IClub
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Fullname();
+    }
+    class Program
+    {
+        class Member : IClub
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public Member()
+            {
+                Id = 0;
+                FirstName = string.Empty;
+                LastName = string.Empty;
+            }
+            public Member(int id, string firstName, string lastName)
+            {
+                Id = id;
+                FirstName = firstName;
+                LastName = lastName;
+            }
+            public string Fullname()
+            {
+                return FirstName + " " + LastName;
+            }
+            public string Display()
+            {
+                return Id + ". " + Fullname();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Member john = new Member(1, "John", "Doe");
+            Console.WriteLine(john.Display());
+            Console.ReadLine();
+        }
+    }
+}
